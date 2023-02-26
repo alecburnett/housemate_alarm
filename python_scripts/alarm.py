a02c4cb856e36d6b73dfc9b74317e412567fccb3
alecburnett <alexburnett1991@hotmail.co.uk>
2023-02-26 15:02:02 +0000

    fixed alarm

--- python_scripts/alarm.py
+++ python_scripts/alarm.py
@@ -2,9 +2,11 @@
 
 import os
 import random
-from playsound import playsound  # requires 'playsound' package
+from pydub import AudioSegment
+from pydub.playback import play
 
-directory = 'mp3_files'
+
+directory = '/home/alexanderjmburnett1991/git/housemate_alarm/mp3_files'
 
 def play_random_mp3(directory):
     # Get a list of all the MP3 files in the directory
@@ -13,5 +15,12 @@ def play_random_mp3(directory):
     # Choose a random MP3 file from the list
     random_file = random.choice(mp3_files)
 
+    print(random_file)
+
+    song = AudioSegment.from_mp3(random_file)
+
     # Use the 'playsound' package to play the MP3 file
-    playsound(random_file)
+    play(song)
+
+
+play_random_mp3(directory)
